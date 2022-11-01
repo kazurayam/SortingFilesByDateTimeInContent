0bf83ff041619669864aeca32a801facd3830794
kazurayam <kazuaki.urayama@gmail.com>
2022-11-02 07:08:02 +0900

    #3 add PathComparableByContentEmailHeaderValue class; delete PathComparableByContentEmailDate class

---
+++ src/main/java/com/kazurayam/study20221030/PathComparableByContentEmailHeaderValue.java
@@ -0,0 +1,13 @@
+package com.kazurayam.study20221030;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PathComparableByEmailHeaderValue
+        extends AbstractPathComparableByDateTime
+        implements IPathComparableByContent {
+
+    private static final Logger logger = LoggerFactory.getLogger(PathComparableByEmailHeaderValue.class);
+
+}
